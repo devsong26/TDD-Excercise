c3c2f0282f70082999ee979b2d428a97be167501
SONGINGWON <devsong26@gmail.com>
2022-09-01 22:08:38 +0900

    회원 가입 테스트 - 7

--- src/main/java/exercise/tdd/simple/member/impl/MemberServiceImpl.java
+++ src/main/java/exercise/tdd/simple/member/impl/MemberServiceImpl.java
@@ -7,12 +7,16 @@ import exercise.tdd.simple.member.ServiceValidator;
 public class MemberServiceImpl extends ServiceValidator<Member> implements MemberService {
     @Override
     public Member register(String username, String password) {
-        validate(new Member(username, password));
+        Member m = new Member(username, password);
+        validate(m);
+
         return null;
     }
 
     @Override
     protected void validate(Member m) {
+        //TODO 기가입 회원 있는지 확인
+
         m.validate();
     }
 
