b8c2b549c3df212410bd9cd43a140dc0e190be8f
SONGINGWON <devsong26@gmail.com>
2022-09-02 21:00:02 +0900

    회원 가입 테스트 - 8 / DataValidator

--- src/main/java/exercise/tdd/simple/member/DataValidator.java
+++ src/main/java/exercise/tdd/simple/member/DataValidator.java
@@ -1,7 +1,27 @@
 package exercise.tdd.simple.member;
 
-public interface DataValidator {
 
-    void validate();
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class DataValidator {
+
+    protected final RegexChecker rgxChecker = new RegexChecker();
+
+    protected abstract void validate();
+
+    protected final RegexChecker getRgxChecker(){
+        return this.rgxChecker;
+    }
+
+    protected static final class RegexChecker {
+        void check(String regex, String data){
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(data);
+
+            if(!m.find())
+                throw RegexException.newEx("The data does not match the regular expression.");
+        }
+    }
 
 }
